7302bf64b62793d1074f71fa71847535958f6bbe
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-07-08 15:07:37 -0500

    added routes

---
+++ src/components/container.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Nav from './Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+export default function Container() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+       {renderPage()}
+    </div>
+  );
+}
