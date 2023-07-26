678e4ba9735a81d264ec8f96a8ff4bad1179b46c
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-07-25 21:07:37 -0500

    added project

--- 00portfolio/src/components/pages/Projects.js
+++ 00portfolio/src/components/pages/Projects.js
@@ -10,6 +10,8 @@ export default function Projects() {
         <li><a href="https://danielkkostich.github.io/random-password-generator/" >Simple Password Generator</a> </li>
         <br></br>
         <li><a href="https://freshair.herokuapp.com/">Collaboration App</a> </li>
+        <br></br>
+        <li><a href="https://powerful-depths-12044-ebf21867d779.herokuapp.com/">Narxaleptics</a> </li>
       </ul>
     </div>
   );
